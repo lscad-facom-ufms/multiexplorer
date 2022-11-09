1c92da62f84c7f4ce79b784794d61b377ec62b17
Erick Rocha Amorim <erochaaamorim@gmail.com>
2022-11-09 15:02:28 -0400

    atualizar construção do sniper

--- MultiExplorer/src/config.example.py
+++ MultiExplorer/src/config.example.py
@@ -4,7 +4,7 @@ PATH_ME = '/multiexplorer/MultiExplorer'  # dir in the virtual environment (Dock
 # PATH_REPO = '/your/local/dir/multiexplorer'  # dir in your physical machine (native set up)
 # PATH_ME = '/your/local/dir/multiexplorer/MultiExplorer'  # dir in your physical machine (native set up)
 
-PATH_SNIPER = PATH_REPO + '/sniper-7.4'
+PATH_SNIPER = PATH_REPO + '/snipersim-8.0'
 PATH_RUNDIR = PATH_REPO + '/rundir'
 PATH_INPUTS = PATH_REPO + '/input-examples'
 
